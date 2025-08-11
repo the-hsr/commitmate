7e75f15cca88064ff76bb45c3219693a5a90547f
the-hsr <himanshu.arunkumar.singh@gmail.com>
2025-08-11 20:37:24 +0530

    Stop tracking out folder

--- out/constants/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Config = void 0;
-exports.Config = {
-    DEFAULT_MODEL: "llama3-8b-8192",
-    TEMPERATURE: 0.5,
-    MAX_TOKENS: 150,
-};
-//# sourceMappingURL=config.js.map
